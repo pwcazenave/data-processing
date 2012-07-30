dbb20a8a9b16705bc5e16a1cf6d17b26b7968c28
Pierre Cazenave <pwcazenave@gmail.com>
2012-07-30 12:04:31 +0100

    Fix a couple of typos and change the long names of the originators. Also use the edited SHOM metadata (which has filled blank fields)

--- proc/tides/add_originator.py
+++ proc/tides/add_originator.py
@@ -9,13 +9,13 @@ c = con.cursor()
 metaDataFiles = ('/users/modellers/pica/Work/data/NSTD/shelf_stations_latlong_sql.csv',\
                  '/users/modellers/pica/Work/data/NTSLF/shelf_stations_sql.csv',\
                  '/users/modellers/pica/Work/data/NTSLF/BPR/shelf_stations_sql.csv',\
-                 '/users/modellers/pica/Work/data/SHOM/shelf_stations_sql.csv')
+                 '/users/modellers/pica/Work/data/SHOM/shelf_stations_sql_edited.csv')
 
 # Add two new fields to the Stations tables in the database
 try:
     # New fields have to be added one at a time
     c.execute('ALTER TABLE Stations ADD COLUMN originatorName TEXT COLLATE nocase')
-    c.execute('ALTER TABLE Stations ADD COLUMN originatorLongName TEXT COLLATE nocase)')
+    c.execute('ALTER TABLE Stations ADD COLUMN originatorLongName TEXT COLLATE nocase')
 except:
     print 'Tables already exist'
 
@@ -27,13 +27,13 @@ for file in metaDataFiles:
         originatorLong = 'North Sea Tidal Data'
     elif 'NTSLF' in file:
         originator = 'NTSLF'
-        originatorLong = 'National Tide and Sea Level Facility Data'
+        originatorLong = 'National Tide and Sea Level Facility'
     elif 'NTSLF/BPR' in file:
         originator = 'BPR'
-        originatorLong = 'Historical Bottom Pressure Sensor Recorder Data'
+        originatorLong = 'Historical Bottom Pressure Sensor Recorder'
     elif 'SHOM' in file:
         originator = 'SHOM'
-        originatorLong = 'Service Hydrographique et Oceanographique de la Marine Data'
+        originatorLong = 'Service Hydrographique et Oceanographique de la Marine'
     else:
         print 'Unknown originator. Skipping.'
         break
