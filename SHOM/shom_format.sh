f8a8b7fcbaed517ba6296815e902325296b0fad6
Pierre Cazenave <pwcazenave@gmail.com>
2012-08-03 14:01:00 +0100

    Format the SHOM data for use as input into the SQLite database

---
+++ SHOM/shom_format.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Take the SHOM raw data, cut out the appropriate columns. Keep single file per
+# station to make loading to the SQL database easier. Add pass flag (P) so that
+# the number of columns matches that in the NTSLF data. There's no QA on the
+# SHOM data, so we're just assuming all data are P.
+
+for i in raw_data/TOTAL/*.slv; do
+    cut -c1-4,6-7,9-10,12-13,15-16,18-19,20-27,28- --output-delimiter="," $i | \
+        tr -d " " | awk -F, '{if (NF==7) print $0",-9999,P"; else print $0",P"}' > ./formatted/$(basename $i)
+done
