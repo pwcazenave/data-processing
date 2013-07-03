d5c8bf8640f32272bbbad3277c8dc917655cdc4b
Pierre Cazenave <pwcazenave@gmail.com>
2013-07-03 11:20:26 +0100

    Add script to get the full time stamps for each station

---
+++ BODC/ctd/get_full_times.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to extract the full times from the lst files for each site (necessary for comparisons with model output).
+
+echo site,year,month,day,hour,minute,second > all_times.txt
+
+for file in raw_data/*.lst; do
+    fulldate=$(grep start: $file | cut -f2 -d:)
+    echo $(basename $file .lst),${fulldate:0:4},${fulldate:4:2},${fulldate:6:2},${fulldate:8:2},${fulldate:10:2},${fulldate:12:2} >> all_times.txt
+done
