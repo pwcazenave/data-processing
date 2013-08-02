b2dc9c65c059037c38769350be59888d15a9421c
Pierre Cazenave <pwcazenave@gmail.com>
2013-08-02 09:41:50 +0100

    Script to get the start and end dates from the ASCII data files

--- BODC/ctd/get_full_times.sh
+++ BODC/ctd/get_full_times.sh
@@ -6,6 +6,10 @@
 echo site,year,month,day,hour,minute,second > all_times.txt
 
 for file in raw_data/*.lst; do
-    fulldate=$(grep start: $file | cut -f2 -d:)
-    echo $(basename $file .lst),${fulldate:0:4},${fulldate:4:2},${fulldate:6:2},${fulldate:8:2},${fulldate:10:2},${fulldate:12:2} >> all_times.txt
+    startdate=$(grep start: $file | cut -f2 -d:)
+    enddate=$(grep end: $file | cut -f3 -d:)
+    if [ -z $enddate ]; then
+        enddate=$startdate
+    fi
+    echo $(basename $file .lst),${startdate:0:4},${startdate:4:2},${startdate:6:2},${startdate:8:2},${startdate:10:2},${startdate:12:2},${enddate:0:4},${enddate:4:2},${enddate:6:2},${enddate:8:2},${enddate:10:2},${enddate:12:2} >> all_times.txt
 done
