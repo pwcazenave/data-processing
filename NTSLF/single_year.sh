adf4d0edb0511814917959ca9509c7578e854013
Pierre Cazenave <pwcazenave@gmail.com>
2013-10-31 15:01:51 +0000

    Update the script to use the compressed files instead of the original uncompressed ones.

--- NTSLF/single_year.sh
+++ NTSLF/single_year.sh
@@ -2,8 +2,8 @@
 
 # Create files for all available years for a single station.
 
-sites=($(\ls -1 formatted/*.txt | cut -f2 -d'/' | tr -d "[0-9]" | cut -f1 -d. | sort -u))
+sites=($(\ls -1 formatted/???????.txt.bz2 | cut -f2 -d'/' | tr -d "[0-9]" | cut -f1 -d. | sort -u))
 
 for ((i=0; i<${#sites[@]}; i++)); do
-    cat formatted/????${sites[i]}.txt | grep -v '  ' > ./formatted/${sites[i]}.txt
+    pbzcat formatted/????${sites[i]}.txt.bz2 | grep -v '  ' > ./formatted/${sites[i]}.txt
 done
