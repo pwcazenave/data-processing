7d3c29d6c38889df32bebd839b40db9ced9d41fd
Pierre Cazenave <pica@pml.ac.uk>
2015-03-27 17:42:21 +0000

    Improve the code to extract positions from the readme.

--- CEFAS/WaveNet/get_positions.sh
+++ CEFAS/WaveNet/get_positions.sh
@@ -11,8 +11,16 @@ mv s.txt sites.txt
 
 grep 'conditions at' ReadMe.htm | cut -f6,7 -d' ' | sed "s/&deg;/\ /g;s/'//g;s/,//g;s/N/\ N\ 1/g;s/S/\ S\ -1/g;s/W/\ W\ -1/g;s/E/\ E\ 1/g" > positions.txt
 
-echo "latDD,lonDD,name" > locations.csv
-paste -d, <(awk '{OFS=","}{print ($1+($2/60))*$4, ($5+($6/60))*$8}' positions.txt) sites.txt >> locations.csv
+paste -d, <(awk '{OFS=","}{print ($1+($2/60))*$4, ($5+($6/60))*$8}' positions.txt) sites.txt >> locs.csv
 
-rm sites.txt positions.txt
+while read line; do
+    if [ -f ./raw_data/$(echo $line | cut -f3 -d, | tr [A-Z] [a-z] | tr -d "\"" | tr " " "-").csv ]; then
+        echo "$line" >> exist.csv
+    fi
+done < locs.csv
+
+echo "latDD,lonDD,name" > locations.csv
+cat exist.csv >> locations.csv
+
+rm sites.txt positions.txt locs.csv exist.csv
 
