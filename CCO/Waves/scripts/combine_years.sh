93f69c5b1c069aaf33976d5b3247f8d91c94cf88
Pierre Cazenave <pica@pml.ac.uk>
2015-07-09 16:58:48 +0100

    Fix path.

--- CCO/Waves/scripts/combine_years.sh
+++ CCO/Waves/scripts/combine_years.sh
@@ -6,6 +6,6 @@ sites=($(ls -1 raw_data/*.txt | cut -f2 -d_ | cut -f2 -d\/ | sort -u))
 
 for ((i=0; i<${#sites[@]}; i++)); do
     echo -n "Working on site ${sites[i]}..."
-    cat raw_data/${sites[i]}*.txt > annual/${sites[i]}.txt
+    cat raw_data/${sites[i]}*.txt > combined/${sites[i]}.txt
     echo "done."
 done
