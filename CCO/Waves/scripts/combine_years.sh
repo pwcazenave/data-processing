8623dd65332a20b4e672b3c9e25e368c1d44c32b
Pierre Cazenave <pica@pml.ac.uk>
2015-07-13 15:26:44 +0100

    Omit the headers for each file when concatenating.

--- CCO/Waves/scripts/combine_years.sh
+++ CCO/Waves/scripts/combine_years.sh
@@ -5,7 +5,11 @@
 sites=($(ls -1 raw_data/*.txt | cut -f2 -d_ | cut -f2 -d\/ | sort -u))
 
 for ((i=0; i<${#sites[@]}; i++)); do
-    echo -n "Working on site ${sites[i]}..."
-    cat raw_data/${sites[i]}*.txt > formatted/${sites[i]}.txt
+    echo -n "Working on site ${sites[i]}... "
+    raw=(raw_data/${sites[i]}*.txt)
+    head -n1 ${raw[0]} > clean/${sites[i]}.txt
+    for f in ${raw[@]}; do
+        tail -q -n +2 $f >> clean/${sites[i]}.txt
+    done
     echo "done."
 done
