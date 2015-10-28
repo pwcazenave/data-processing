f06ded4140f08b1b524f8fcd02b10441fa6c7363
Pierre Cazenave <pica@pml.ac.uk>
2015-10-28 11:16:35 +0000

    Add the new data and adjust the script to convert the time series from pressure to depth accordingly. Some of the new data are not actually pressure time series, so just extract the surface elevation time series only for those locations. Since these data are only used for harmonic analysis, this should not be a problem as I presume I remove the mean depth anyway before performing the analysis.

--- NTSLF/BPR/pressure2depth.sh
+++ NTSLF/BPR/pressure2depth.sh
@@ -3,7 +3,7 @@
 # Use the hydrostatic pressure to calculate an equivalent depth. Add nonsense
 # residual and quality flags too (as per SHOM approach).
 
-files=(./raw_data/*.dat)
+files=(./raw_data/*.dat ./raw_data/*.lst)
 
 for ((i=0; i<${#files[@]}; i++)); do
     # Convert mbar -> bar -> Pa, then calcualte depth from hydrostatic
@@ -12,7 +12,24 @@ for ((i=0; i<${#files[@]}; i++)); do
     #           g = 9.81 m/s2
     #           P = pressure in Pascals
     #           z = depth in metres
-    awk '{if (NR>13) print $2,$3,(($4/1000)*1e5)/(1026*9.81)}' ${files[i]} | \
-        tr "/" " " | awk -F. '{OFS=","}{print $1,$2,$3"."$4,"-9999","P"}' | \
-        tr " " "," > ./formatted/$(basename ${files[i]})
+    if [ ${files[i]##*.} == 'lst' ]; then
+        header=20
+    else
+        header=13
+    fi
+    echo -n "${files[i]}: $header "
+    if grep -qi pressure ${files[i]}; then
+        echo "pressure"
+        awk '{if (NR>'$header') print $2,$3,(($4/1000)*1e5)/(1026*9.81)}' ${files[i]} | \
+            tr "/" " " | tr ":" "." | awk -F. '{OFS=","}{print $1,$2,$3"."$4,"-9999","P"}' | \
+            tr " " "," > ./formatted/$(basename ${files[i]})
+    else
+        echo "elevation"
+        # We don't have pressure (must have surface elevation). These records
+        # don't have a site depth, so just extract the surface elevation
+        # timeseries.
+        awk '{if (NR>'$header') print $2,$3,$4}' ${files[i]} | \
+            tr "/" " " | tr ":" "." | awk -F. '{OFS=","}{print $1,$2,$3"."$4,"-9999","P"}' | \
+            tr " " "," > ./formatted/$(basename ${files[i]})
+    fi
 done
