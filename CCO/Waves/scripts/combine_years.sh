1e1b604902a5451ed1ece4285fe82d38779b5151
Pierre Cazenave <pica@pml.ac.uk>
2016-01-27 14:31:05 +0000

    Output to the long name instead of the short name (makes the database output easier to use with sensible names).

--- CCO/Waves/scripts/combine_years.sh
+++ CCO/Waves/scripts/combine_years.sh
@@ -5,11 +5,13 @@
 sites=($(ls -1 raw_data/*.txt | cut -f2 -d_ | cut -f2 -d\/ | sort -u))
 
 for ((i=0; i<${#sites[@]}; i++)); do
-    echo -n "Working on site ${sites[i]}... "
+    # Get its longname so we can use that as the key for the database.
+    site=$(grep -a ${sites[i]} locations.csv | cut -f3 -d,)
+    echo -n "Working on site ${site}... "
     raw=(raw_data/${sites[i]}*.txt)
-    head -n1 ${raw[0]} > clean/${sites[i]}.txt
+    head -n1 ${raw[0]} > clean/${site}.txt
     for f in ${raw[@]}; do
-        tail -q -n +2 $f >> clean/${sites[i]}.txt
+        tail -q -n +2 $f >> clean/${site}.txt
     done
     echo "done."
 done
